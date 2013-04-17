4e1319ab2460a7c12bf170d13505259dbe57436a
Ivan Borshukov <bo0merzzz@gmail.com>
2013-04-17 15:58:35 +0300

    Added BFS, BFS simple tests

--- algorithms.py
+++ algorithms.py
@@ -4,6 +4,7 @@ from graphs import Graph
 
 
 def dfs(graph, start):
+	''' Runs depth-first search from start node. yields (parent, child) for each visited node. '''
 	if start in graph:
 		stack = deque()
 		visited = deque()
@@ -15,3 +16,22 @@ def dfs(graph, start):
 				if adj_node not in visited:
 					yield node, adj_node
 					stack.append(adj_node)
+
+
+def bfs(graph, start):
+	''' Runs breadth-first serach from start node. yeilds {node: {child_node, child_node2},
+	other_node: {o_child_node, o_child_node2}} '''
+	current_level = [start]
+	visited = {}
+	while current_level:
+		for node in current_level:
+			visited[node] = 1
+		next_level = set()
+		level_graph = {node: set() for node in current_level}
+		for node in current_level:
+			for iter_node in graph[node]:
+				if iter_node not in visited:
+					level_graph[node].add(iter_node)
+					next_level.add(iter_node)
+		yield level_graph
+		current_level = next_level
--- algorithms_test.py
+++ algorithms_test.py
@@ -16,8 +16,25 @@ class AlgorithmsTest(unittest.TestCase):
 
 		self.assertEqual(list(dfs(g, 1)), 
 			[(1, 2), (2, 3), (2, 7), (7, 'Sofia'), (3, 5)])
-		
+
 		self.assertEqual(list(dfs(g, 'Bourgas')), [('Bourgas', 'Varna')])
+
+	def test_bfs(self):
+		g = Graph()
+		g.add_edge(1, 2)
+		g.add_edge(1, 3)
+		g.add_edge(2, 4)
+		g.add_edge(4, 'Sofia')
+		g.add_edge(4, 'Bourgas')
+		g.add_edge(4, 'London')
+		must_be = {0: {1: {2, 3}},
+					1: {2: {4}, 3: set()},
+					2: {4: {'London', 'Sofia', 'Bourgas'}},
+					3: {'London': set(), 'Sofia': set(), 'Bourgas': set()}}
+
+		for level, graph_level in enumerate(bfs(g, 1)):
+			print(graph_level)
+			self.assertEqual(must_be[level], graph_level)
 
 if __name__ == '__main__':
 	unittest.main()
