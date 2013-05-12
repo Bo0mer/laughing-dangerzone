5fc4f2902ac5ea1871b6e47d9e3e96b215cc1f95
Ivan Borshukov <bo0merzzz@gmail.com>
2013-05-12 18:11:42 +0300

    Converted tabs to spaces.

--- algorithms/eulerian.py
+++ algorithms/eulerian.py
@@ -8,14 +8,14 @@ def is_eulerian(graph):
     """ Returns True if the graph is Eulerian. """
 
     if graph.is_directed():
-    	if is_connected(graph):
-        	return all((
-        		graph.in_degree(node) == graph.out_degree(node)
+        if is_connected(graph):
+            return all((
+                graph.in_degree(node) == graph.out_degree(node)
                 for node in graph))
     else:
         if is_connected(graph):
             return all((
-            	graph.degree(node) % 2 == 0
+                graph.degree(node) % 2 == 0
                 for node in graph))
     return False
 
